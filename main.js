987f8b3345843833f4bfecb006740bf9d8270b9c
IbrEli <79771027+IbrEli@users.noreply.github.com>
2021-08-09 23:44:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// var, let, const
+
+let score;
+
+score = 10;
+
+console.log(score);
+
+
+// Data Types: String, Numbers, Boolean, null, undefined, Symbol
+
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+// Strings
+// Concatenation
+console.log('My name is ' + name + ' and I am ' + age);
+// Template String
+console.log(`My name is ${name} and I am ${age}`);
+
+const s = 'Hello World';
